760ba67777480871b38599b492e29e4971a186f6
Anup Maurya <anupmaurya3336@gmail.com>
2026-06-24 23:32:21 +0530

    FInal code

--- server/server.js
+++ server/server.js
@@ -19,7 +19,11 @@ const app = express()
 const PORT = process.env.PORT || 4000;
 
 //Middleware
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 app.use(express.json())
 app.use(multer().none())
 
